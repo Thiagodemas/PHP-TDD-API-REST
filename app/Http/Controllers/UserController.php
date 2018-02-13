a23f57ca6eb28f7e9da264839e63b52e8efcb998
thiagodemas <thiago.demas7@gmail.com>
2018-02-12 22:36:54 -0300

    create tests UserTest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,14 +10,23 @@ class UserController extends Controller
 
     public function __construct()
     {
-
+        //
     }
 
     public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|max:255'
+        ]);
         $user = new User($request->all());
         $user->save();
         return $user;
     }
 
-    //
+    public function view($id){
+        return User::find($id);
+    }
+
+
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,10 +1,11 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseMigrations;
+
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class UserTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -14,13 +15,35 @@ class UserTest extends TestCase
     {
         $dados = [
             'name' => 'Name 01',
-            'email' => 'email@exemplo.com',
+            'email' => 'email5@exemplo.com',
             'password' => '12345'
         ];
+
         $this->post('/api/user', $dados); //testar primeiro medoto 'creater User'
+
+
         $this->assertResponseOk(); //vai retornar se a pagina existe
 
+        echo $this->response->content();
+
         $resposta = (array) json_decode($this->response->content());
+
+        $this->assertArrayHasKey('name', $resposta); //ele vai procurar se na resposta tem algum campo com o nome 'name'
+        $this->assertArrayHasKey('email', $resposta);
+        $this->assertArrayHasKey('id', $resposta);
+    }
+
+    public function testViewUser()
+    {
+        $user = \App\User::first();
+
+        $this->get('/api/user/'.$user->id); //testar primeiro medoto 'creater User'
+
+        $this->assertResponseOk(); //vai retornar se a pagina existe
+
+
+        $resposta = (array) json_decode($this->response->content());
+
         $this->assertArrayHasKey('name', $resposta); //ele vai procurar se na resposta tem algum campo com o nome 'name'
         $this->assertArrayHasKey('email', $resposta);
         $this->assertArrayHasKey('id', $resposta);
